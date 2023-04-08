9a169dabc65f6e36d486e94ca6ab127e71fd361f
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-08 11:42:17 +0530

    Update ArrayList.java

--- Till ST1/Intro To Java/ArrayList.java
+++ Till ST1/Intro To Java/ArrayList.java
@@ -3,6 +3,19 @@ import java.util.*;
 // lets learn how to create and print a 'ArrayList'
 // note : array is different then ArrayList, where array is just a basic array with indicies, whereas ArrayList is a part of java collection frameword with methods like .add(), .iterator() etc.
 
+/* ⭐(Ques) Difference between array, list, and ArrayList in Java ?
+
+-> An array is a basic collection of fixed size that can store any declared type of
+elements. 
+
+-> The List is a dynamic collection (it means, its size can change over
+time) that is generic, so it can also store any declared type of elements.
+
+-> An ArrayList is a dynamic collection that can store various types of elements at 
+the same time, as it treats everything it stores as instances of the System.Object
+type
+
+*/
 class ArrayList {
     public static void main(String[] args){
 
