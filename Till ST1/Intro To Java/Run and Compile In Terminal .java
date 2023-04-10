fa1e4dfa758e516834a84aea2f33a814846389c2
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-10 09:50:08 +0530

    Create Run and Compile In Terminal .java

---
+++ Till ST1/Intro To Java/Run and Compile In Terminal .java
@@ -0,0 +1,16 @@
+
+// create a java file with extension .java
+public class New{
+    
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
+
+// now open the terminal and go to the folder where this above file is stored 
+PS C:\Users\Yashasvi\Downloads> javac New.java  // to compile a file
+PS C:\Users\Yashasvi\Downloads> java New   // to run a file
+Hello Java // output 
+  
+  
+
