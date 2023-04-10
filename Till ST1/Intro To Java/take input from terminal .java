1869aa724b8b02fbe2446e78bcc37afc1f6d8f9e
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-10 10:18:59 +0530

    Create take input from terminal .java

---
+++ Till ST1/Intro To Java/take input from terminal .java
@@ -0,0 +1,18 @@
+// program to add 2 numbers 
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String args[]){
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
+		
+		System.out.println("Sum = " + (n1+n2));
+    }
+    
+}
+
+// lets compile and run
+PS C:\Users\Yashasvi\Downloads> javac Calculator.java
+PS C:\Users\Yashasvi\Downloads> java Calculator 10 20  // last 2 are inputs for the program 
+Sum = 30
