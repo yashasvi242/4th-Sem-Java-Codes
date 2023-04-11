bc53c2cf1a2f37761a4597a47d0c89594a9bfa58
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-11 10:58:18 +0530

    Create typeCasting .java

---
+++ Till ST1/Intro To Java/typeCasting .java
@@ -0,0 +1,32 @@
+class New{
+
+	public static void main(String[] args){
+
+		double d = 100;
+		float f = (float)d;
+		long L = (long)f;
+		int i = (int)L;
+		short s = (short)i;
+		byte b = (byte)s;
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(L);
+		System.out.println(i);
+		System.out.println(s);
+
+
+	}
+
+	
+
+}
+// output : 
+/*
+
+100.0
+100.0
+100
+100
+100
+
+*/
