dff8e445891760cc58ffadd7ff6d8092db02a798
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-18 11:12:19 +0530

    Update 4. methods calling with string.java

--- Till ST1/Intro To Java/Classes & Object/4. methods calling with string.java
+++ Till ST1/Intro To Java/Classes & Object/4. methods calling with string.java
@@ -1,6 +1,7 @@
 public class MyClass {
   
   // concat string 
+  // make sure to use 'static' or 'static public' for all methods declared in class in case u are using offline compiler like VSCode 
     
     String name;
     
