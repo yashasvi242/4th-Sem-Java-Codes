6f3c247177f2e7178f64de503796ea2bbe3a1e0e
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-18 16:07:52 +0530

    Create 10. implicit typecasting.java

---
+++ Till ST1/Intro To Java/Classes & Object/10. implicit typecasting.java
@@ -0,0 +1,21 @@
+
+class Demo{
+
+	void sum(int l, long b){
+		System.out.println("Sum is " + (l+b));
+	}
+
+	void sum(int l, int b, int h){
+		System.out.println("Sum is " + (l+b+h));
+	}
+
+	public static void main(String[] args){
+		Demo ar = new Demo();
+		ar.sum(8, 5); // note : we have passed integer 5 but it will be comverted to long (implicitely) by the function parameter datatype itself, this is called 
+	}
+}
+/*
+Output 
+Sum is 13
+
+*/
