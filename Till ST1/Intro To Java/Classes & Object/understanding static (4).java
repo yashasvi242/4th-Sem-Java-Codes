b772a8a204b5d4ed20eeab40c6003de4799e7ae9
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-19 14:33:59 +0530

    Update understanding static (4).java

--- Till ST1/Intro To Java/Classes & Object/understanding static (4).java
+++ Till ST1/Intro To Java/Classes & Object/understanding static (4).java
@@ -8,7 +8,13 @@ public class MyClass {
     
     public static void main(String args[]) {
         
+        // M-1
         square(4); // static method is called with/without class name 
+        
+        //M-2
+        MyClass.square(4);  // this also works fine
+        
+        //M-3 : to create an object of 'MyClass' then access the method square()
     }
 }
 
