72eb3c2d8ce2ae3fb45e10bdfb63e6dfd1296adf
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-20 15:38:35 +0530

    Update inheritance(3).java

--- Till ST1/Intro To Java/Inheritance/inheritance(3).java
+++ Till ST1/Intro To Java/Inheritance/inheritance(3).java
@@ -1,1 +1,44 @@
 
+//Inheritance (3)
+
+// Multi level Inheritance 
+
+ 
+class A{//parent
+    String a = "Class A";
+    void showA(){
+        System.out.println(a);
+    }
+}
+
+class B extends A{ //child
+    String b = "Class B";
+    void showB(){
+        System.out.println(b);
+    }
+}
+
+public class C extends B{ //grand child
+    public static void main(String[] args){
+        C c = new C();
+        c.showA();
+        c.showB();
+    }
+}
+
+
+/* Output : 
+
+Class A
+Class B
+
+*/
+
+
+
+
+
+
+
+
+
