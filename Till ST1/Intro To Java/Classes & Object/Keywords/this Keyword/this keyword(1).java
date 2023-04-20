393e08e73f726d50425e087e94cdac9489df8005
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-20 10:57:24 +0530

    Create this keyword(1).java

---
+++ Till ST1/Intro To Java/Classes & Object/Keywords/this Keyword/this keyword(1).java
@@ -0,0 +1,37 @@
+public class Demo {
+    
+    // by default uninitialized 'double' has value null.
+    // this stores the reference of curr object.
+    
+    Double width, height, depth;
+    Demo(double w, double h, double d) {
+        
+        Double width=0d, height = 0d, depth = 0d;  // local variable
+        this.width = w;
+        this.height = h;
+        this.depth = d;
+        System.out.println("Constwidth = " + width);  // local variables will be printed
+        System.out.println("constheight = " + height);
+        System.out.println("Constdepth = " + depth);
+    }
+    
+    public static void main(String[] args){
+        Demo d = new Demo(10,20,30);
+        System.out.println("width = " + d.width);  // obj's variables will be printed
+        System.out.println("heigh = " + d.height);
+        System.out.println("depth = " + d.height);
+    } 
+}
+
+/*
+Output :- 
+
+Constwidth = 0.0
+constheight = 0.0
+Constdepth = 0.0
+width = 10.0
+heigh = 20.0
+depth = 20.0
+
+
+*/
