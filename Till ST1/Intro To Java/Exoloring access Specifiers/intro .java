3ab11c2928d1345df4fce54548445fcb833d6ff4
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 13:49:23 +0530

    Create intro .java

---
+++ Till ST1/Intro To Java/Exoloring access Specifiers/intro .java
@@ -0,0 +1,10 @@
+                            Sane package                        Other Package 
+            Same class    Child ckass non child                 Child class   Non child class
+    
+public       YES            Yes   Yes                               Yes       Yes 
+    
+protected    YES            Yes   Yes                               Yes       No
+    
+Default      YES            Yes   Yes                               No        No
+
+Private      YES            No    No                                No        No 
