91972089ca5d6e2b9226fff58fa6355921a2629d
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 11:14:52 +0530

    Update cmp decimal digits of 2 doubles.java

--- Till ST1/Programs/cmp decimal digits of 2 doubles.java
+++ Till ST1/Programs/cmp decimal digits of 2 doubles.java
@@ -11,45 +11,49 @@ public class main{
     
     public static void main(String args[]){
         
-        // double n1 = 200.26;
-        // double n2 = 10.26;
+        double n1 = 200.26;
+        double n2 = 10.26;  // test case 1 
         
-        double n1 = 200.35;
-        double n2 = 20.23;
+        // double n1 = 200.35;  // test case 2 
+        // double n2 = 20.23;
         
-        // check decimal are same or not 
         int num1 = (int)n1;
         int num2 = (int)n2;
         
-        double number1 = (n1%num1) + 1;
-        double number2 = (n2%num2) + 1;
+        double number1 = (n1%num1) ;  // fetching out decimals only
+        double number2 = (n2%num2) ;
         
-        System.out.println(number1 + " " + number2);  //0.2599999999999909 0.2599999999999998
+        //System.out.println(number1 + " " + number2);  //0.2599999999999909 0.2599999999999998
         
         // lets store doubles n1 and n2 upto only 2 decimal places. eg 0.25 for above written comment. 
         
-        /*
-            //M-1 (works fine)
-            
+        
+        //M-1 (works fine)
         double r1 = Math.round(number1*100.00)/100.00;
         double r2 = Math.round(number2*100.00)/100.00; 
         
-        */
         
+        
+        /*
         // M-2 (works fine) 
         double r1 = (double)Math.round(number1*100)/100;
         double r2 = (double)Math.round(number2*100)/100;
+        */
         
-        if(r1 == r2) 
+        if(r1 == r2) {
+            System.out.println(r1 + " == " + r2);
             System.out.println("Yes, Same decimal digits");
-        else 
+        }
+        else{
+            System.out.println(r1 + " != " + r2);
             System.out.println("No, Not Same");
-         
+        }
     }
 }
 
-/*
-1.3499999999999943 1.2300000000000004
-No, Not Same
+/* Output :- 
+
+0.26 == 0.26
+Yes, Same decimal digits
 
 */
