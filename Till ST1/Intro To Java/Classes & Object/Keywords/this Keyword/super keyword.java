f0c947a7c3e721d3e3ae1852e8f53c54f85edbf5
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 09:23:27 +0530

    Create super keyword.java

---
+++ Till ST1/Intro To Java/Classes & Object/Keywords/this Keyword/super keyword.java
@@ -0,0 +1,25 @@
+class Parent{
+    String name;
+}
+
+public class Child extends Parent{
+    String name;
+    public void details(){
+        super.name = "Parent";
+        name = "Child";
+        System.out.println(super.name + " and " + name);
+    }
+    
+    public static void main(String[] args){
+        Child cobj = new Child();
+        cobj.details();
+    } 
+}
+
+/*
+
+Output 
+
+Parent and Child
+
+*/
