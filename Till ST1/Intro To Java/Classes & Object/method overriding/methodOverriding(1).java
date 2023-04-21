36cbfe8ad2b4690fd98641711fc6abb655637978
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 14:02:21 +0530

    Create methodOverriding(1).java

---
+++ Till ST1/Intro To Java/Classes & Object/method overriding/methodOverriding(1).java
@@ -0,0 +1,30 @@
+
+// method overloading means methods having same name but diff number of arguments, 
+// method Overriding means same method name and same parameter, occur in different class that has inheritance relationship. we use method overriding to implement specific functionality to the method.
+
+
+class Animal{
+    public void eat(){
+        System.out.println("Eat all eatables");
+    }
+}
+
+public class Dog extends Animal{
+    public void eat(){
+        System.out.println("Dog likes to eat meat");
+    }
+    
+    public static void main(String args[]){
+        Dog d = new Dog();
+        d.eat();
+        Animal a = new Animal();
+        a.eat();
+    }
+}
+
+/*
+Output :- 
+
+Dog likes to eat meat
+Eat all eatables
+*/
