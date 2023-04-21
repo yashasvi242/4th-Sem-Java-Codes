53644fc69ec6e73d5496d3227ecbd62488bd17d8
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 11:47:26 +0530

    Update rounding of till 'n' decimals.java

--- Till ST1/Programs/predefined methods/rounding of till 'n' decimals.java
+++ Till ST1/Programs/predefined methods/rounding of till 'n' decimals.java
@@ -1,5 +1,5 @@
 
-// converting double to double with 3 decimal places. (using String.format())
+// converting double to double with 3 decimal places.
 
 public class Main {
     public static String convertTill3deci(double cnum1){
@@ -16,22 +16,18 @@ public class Main {
         // converting 'double' to 'String' with 3 decimal places
         String cnum1 = convertTill3deci(num1); // works fine
         
+        //m-1 (works fine)
         // converting 3 decimal placed string into 'double' again 
-        double number = Double.parseDouble(cnum1); // converting string to double (indirectly ) coz there is no direct way 
+        //double number = Double.parseDouble(cnum1); // converting string to double (indirectly ) coz there is no direct way 
+        
+        //m-2 (prefered method)
+        Double number = Double.valueOf(cnum1);
         
         // printing the final double 
         System.out.println(number);
         
         
         
- 
+        
     }
 }
-
-
-/*
-
-num1 : 10.494589
-10.495
-
-*/
