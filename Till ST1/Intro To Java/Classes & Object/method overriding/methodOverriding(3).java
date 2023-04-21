34db0ddce59126101a98daaa70052af089b58559
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 14:13:15 +0530

    Create methodOverriding(3).java

---
+++ Till ST1/Intro To Java/Classes & Object/method overriding/methodOverriding(3).java
@@ -0,0 +1,31 @@
+//method overriding with access specifiers (2)
+
+// note : for inherited classes both the parent and childs should have same access specifier for same named functions and that too only 'public' and 'default'
+
+class Animal{
+    public void eat(){
+        System.out.println("Eat all eatables");
+    }
+}
+
+public class Dog extends Animal{
+    private void eat(){
+        System.out.println("Dog likes to eat meat");
+    }
+    
+    public static void main(String args[]){
+        Dog d = new Dog();
+        d.eat();
+        Animal a = new Animal();
+        a.eat();
+    }
+}
+
+/*
+Output :- 
+
+Line 10: error: eat() in Dog cannot override eat() in Animal [in Dog.java]
+    private void eat(){
+                 ^
+  attempting to assign weaker access privileges; was public
+*/
