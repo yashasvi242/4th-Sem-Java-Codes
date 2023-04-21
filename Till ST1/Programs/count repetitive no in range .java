1e28ad6f8657db5851668924d3e0cf12e5505054
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-21 15:11:49 +0530

    Create count repetitive no in range .java

---
+++ Till ST1/Programs/count repetitive no in range .java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main{
+   
+    /*  count number of repeatitive numbers from given range
+   
+    Test Case 1 :
+        //input 11 15
+        //output 4
+       
+    Test Case 2 :
+        input 101 200
+        output 72
+   
+    */
+   
+    public static void main(String args[]){
+        int start = 11;
+        int end = 15;
+        int repeatitiveCount = 0;
+       
+        for(int i=start; i <= end; i++){
+           
+            int num = i;
+            int eleToFreq[] = new int[10]; // for 0 to 9
+           
+            while(num != 0){
+                int lastDig = num % 10;
+                eleToFreq[lastDig]++;
+                num /= 10;
+            }
+           
+            // check freq
+            for(int j=0; j < 10; j++){
+                if(eleToFreq[j] > 1){
+                    repeatitiveCount++;
+                    break;
+                }
+            }
+        }
+        int nonRepeatitive = end - start + 1 - repeatitiveCount;
+        System.out.print("non repeatitive : " + nonRepeatitive);
+       
+    }
+}
+
+//output : 4 
