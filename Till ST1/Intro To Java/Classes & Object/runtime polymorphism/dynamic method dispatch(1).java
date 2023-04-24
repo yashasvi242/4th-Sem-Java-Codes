b9a8490cf14a6e1971140f7cb390a5bca6dcabbb
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-24 09:51:49 +0530

    Create dynamic method dispatch(1).java

---
+++ Till ST1/Intro To Java/Classes & Object/runtime polymorphism/dynamic method dispatch(1).java
@@ -0,0 +1,44 @@
+
+// runtime polymorphism 
+// reference will be of parent 
+// object will be of child always.
+
+class Bank{
+    float getRateOfInterest(){return 0;}
+}
+
+
+class SBI extends Bank{
+    float getRateOfInterest(){return 8.4f;}
+}
+
+
+class ICICI extends Bank{
+    float getRateOfInterest(){return 7.3f;}
+}
+
+class AXIS extends Bank{
+    float getRateOfInterest(){return 9.7f;}
+}
+
+public class TestPolymorphism{
+    public static void main(String args[]){
+        Bank b;
+        b = new SBI();
+        System.out.println("SBI Rate of Interest: " + b.getRateOfInterest() );
+        b = new ICICI();
+        System.out.println("ICICI Rate of Interest: " + b.getRateOfInterest() );
+        b = new AXIS();
+        System.out.println("AXIS Rate of Interest: " + b.getRateOfInterest() );
+    }
+}
+
+/*
+
+SBI Rate of Interest: 8.4
+ICICI Rate of Interest: 7.3
+AXIS Rate of Interest: 9.7
+
+
+*/
+
