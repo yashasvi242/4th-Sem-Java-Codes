6bdefd8c4927a938989bb04f8d03d98c99229c8c
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-24 09:40:09 +0530

    Create dynamic method dispatch.java

---
+++ Till ST1/Intro To Java/Classes & Object/runtime polymorphism/dynamic method dispatch.java
@@ -0,0 +1,33 @@
+
+// runtime polymorphism (dynamic method dispatch)
+// reference will be of parent 
+// object will be of child always.
+
+class Game{
+    public void type(){
+        System.out.println("Indoor & outdoor");
+    }
+}
+
+public class Cricket extends Game{
+    public void type(){
+        System.out.println("outdoor game");
+    }
+    
+    public static void main(String[] args){
+        Game gm = new Game();
+        Cricket ck = new Cricket();
+        gm.type();
+        ck.type();
+        gm = ck;// gm (parent's) reference and ck(child's) object
+        gm.type(); // object is still of child, so chil'd method will be given priority first
+    }
+}
+
+/*
+
+Indoor & outdoor
+outdoor game
+outdoor game
+
+*/
