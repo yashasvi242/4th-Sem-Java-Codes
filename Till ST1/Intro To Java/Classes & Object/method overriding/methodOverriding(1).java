223931f0ef67e854040167d4ea924956bab499d7
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-24 09:53:18 +0530

    Update methodOverriding(1).java

--- Till ST1/Intro To Java/Classes & Object/method overriding/methodOverriding(1).java
+++ Till ST1/Intro To Java/Classes & Object/method overriding/methodOverriding(1).java
@@ -1,7 +1,8 @@
 
 // method overloading means methods having same name but diff number of arguments, 
 // method Overriding means same method name and same parameter, occur in different class that has inheritance relationship. we use method overriding to implement specific functionality to the method.
-
+// Overriding implements Runtime Polymorphism. 
+// Whereas Overloading implements Compile time polymorphism.
 
 class Animal{
     public void eat(){
