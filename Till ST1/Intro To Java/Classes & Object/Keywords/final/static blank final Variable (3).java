fe22da8f8ed757d69277ccae77924e5ef5b03a11
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-25 11:18:21 +0530

    Update static blank final Variable (3).java

--- Till ST1/Intro To Java/Classes & Object/Keywords/final/static blank final Variable (3).java
+++ Till ST1/Intro To Java/Classes & Object/Keywords/final/static blank final Variable (3).java
@@ -1,12 +1,17 @@
 // note : we can initialize the blank final variable inside static block only.
 // note : we can not initialize a 'static final blank variable' inside constuctor 
+// note : only the blank final variable can be initialized inside constuctor, 'static blank final variable' can only be inintialized inside the static block 
 
 public class Demo{
     static final int a; //static blank final variable 
-    // static{
-    //     a=10; // static final blank variable can be initialized inside static block
-    // }
     
+ /*  // correct way 
+    static{
+        a=10; // static final blank variable can be initialized inside static block
+    }
+ */
+    
+    // error - only the blank final variable can be initialized inside constuctor, 'static blank final variable' can only be inintialized inside the static block 
     Demo(){
         a=10;
     }
