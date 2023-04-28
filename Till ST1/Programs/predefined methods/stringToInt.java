d75490756b707f77d399a439878936228259e4db
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-04-28 09:14:57 +0530

    Create stringToInt.java

---
+++ Till ST1/Programs/predefined methods/stringToInt.java
@@ -0,0 +1,18 @@
+
+public class StringToInt {
+    
+    public static void main(String ... args){
+
+        String str = "12345";
+
+        int n = Integer.valueOf(str);
+
+
+        System.out.print("String converted to int : " + n);
+
+    }
+
+
+}
+
+//Output : String converted to int : 12345
