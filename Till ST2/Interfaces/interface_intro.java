ce9d54b5bb7ffac68f2a501ed2e25d747b0a9254
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-03 14:16:40 +0530

    Create interface_intro.java

---
+++ Till ST2/Interfaces/interface_intro.java
@@ -0,0 +1,11 @@
+/*
+  - interface supports multiple inheritance
+  - can not make object of interfaces (just like abstract classes )
+  - keyword : implements
+  - methods inside interface must not be static, final, native (because then we can not change it)
+  - all variabkes declared inside interface are implicitely (by default) public, staitc, and final 
+  - all methods declared inside this are implicitely public and abstract 
+  - interface can extend oe or more other interface.
+  - it can not implement the class 
+
+ */
