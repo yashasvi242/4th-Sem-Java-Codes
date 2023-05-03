fcf24c3d79adae53b36abb6df5c6b479c43f38bd
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-03 19:57:13 +0530

    Update interface_intro.java

--- Till ST2/Interfaces/interface_intro.java
+++ Till ST2/Interfaces/interface_intro.java
@@ -5,8 +5,8 @@
   - methods inside interface must not be static, final, native (because then we can not change it)
   - all variabkes declared inside interface are implicitely (by default) public, staitc, and final 
   - all methods declared inside this are implicitely public and abstract 
-  - interface can extend oe or more other interface.
-  - it can not implement the class 
+  - interface can extend one or more other interface.
+  - interface can not implement a class.
   - interface can inherit another interface using 'extends' 
 
   - abstract classes may or may not contain body of method, but in interface there must not be body, only declaration.
