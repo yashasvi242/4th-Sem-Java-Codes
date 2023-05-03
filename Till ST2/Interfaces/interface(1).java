3a07a809b2ae9fe9ff38cb4b19e2678c674615e7
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-03 20:00:34 +0530

    Update interface(1).java

--- Till ST2/Interfaces/interface(1).java
+++ Till ST2/Interfaces/interface(1).java
@@ -1,12 +1,12 @@
 
 interface Moveable{
     int AVG_SPEED = 40;
-    void move();
+    void move(); // implicitely (by default) every method inside a interface is private and abstract, so to use it, make sure whereever u define its body, use 'public' there
 }
 
 class Interface1 implements Moveable{
 
-    public void move(){
+    public void move(){  // note : if we remove this public, then it will be a private, abstract method (as taught above)
         System.out.println("Average speed is " + AVG_SPEED);
     }
 
