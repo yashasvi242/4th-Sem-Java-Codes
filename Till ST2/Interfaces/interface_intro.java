43b24bdc5952cfc1721f8b71f10685aabb068034
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-03 19:59:43 +0530

    Update interface_intro.java

--- Till ST2/Interfaces/interface_intro.java
+++ Till ST2/Interfaces/interface_intro.java
@@ -3,8 +3,8 @@
   - can not make object of interfaces (just like abstract classes )
   - keyword : implements
   - methods inside interface must not be static, final, native (because then we can not change it)
-  - all variabkes declared inside interface are implicitely (by default) public, staitc, and final 
-  - all methods declared inside this are implicitely public and abstract 
+  - all variabkes declared inside interface are implicitely (by default) private, staitc, and final 
+  - all methods declared inside this are implicitely private and abstract 
   - interface can extend one or more other interface.
   - interface can not implement a class.
   - interface can inherit another interface using 'extends' 
