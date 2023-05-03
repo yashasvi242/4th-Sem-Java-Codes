bc69e5d7cd0837cf5fdd5b562c02c8343ce55031
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-03 10:04:06 +0530

    Create introToPackages.java

---
+++ Till ST2/Packages/introToPackages.java
@@ -0,0 +1,24 @@
+
+/* Understanding Packages :- 
+
+    - Collection of related class
+    - to group related classes, interface and sub pachages
+    Types of pachages 
+        - user defined 
+        - build in : util, Math, lang, i/o
+
+    - pakage statement must be first statement in the program, even before the import statement
+    - package is always defined as a separate folder having the same name as teh package name
+
+    - store all classes in that package folder.
+    - All classes of the package which we wish to access outside the package must be declared public.
+
+ 
+    How to access packages ?
+
+    3 ways :
+        1. without using package keyword - create object using 'pack.A = new pack.A()'
+        2. using package keyword
+        3. importing specific class of the package : import pack.Demo (demo is the class inside package 'pack')
+
+ */
