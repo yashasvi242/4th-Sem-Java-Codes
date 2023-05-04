dfe1ca37710645246cc1a20c932acbfa8c6c1c39
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-04 16:16:58 +0530

    Update strings Intro(0) .java

--- Till ST2/Strings/strings Intro(0) .java
+++ Till ST2/Strings/strings Intro(0) .java
@@ -1,6 +1,8 @@
 import java.lang.*;
 
 /*
+    - Strings are immutable (but can be muted using string buffer)
+
   - String is an object that represents sequence of characters.
   - java.lang packages
   - every string we create in java is an object of type String
@@ -45,8 +47,61 @@ import java.lang.*;
             1. ==  (checks if 2 objects are pointing to same memory or not)
             2. equals() -  (checks if 2 objects have same string data )
             2. CompareTo() - returns diff of asci value of the 2 strings/chars 
-  - 
-  - 
+        
+        String functions :-  
+            1. charAt(index)
+            2. equalsIgnoreCase() - it ignores the case and just checks the 2 string, return true and false
+            3. indexOf() 
+                indexOf(char) - first occ of char
+                indexOf(char, x) - first occ of char starting from x 
+                indexOf(string) - first occ of this string, returns first char's presence
+            
+            4. length()  - number of char in the string (includes spaces)
+            5. str.replace(oldChar, newChar) 
+            6. str.substring()
+            7. str.toLowerCase();
+            8. str.toUpperCase();
+            9. str.valueOf() 
+                // convert int to str (primitive data to string)
+                String s1 = String.value(num);
+                // string to primitive
+                int num1 = parseInt(s1);
+
+            10. toString()
+                    - if we do not override toString() then objs id )hascode
+ will pe printed
+
+            11. str.trim() - removes the space from start and end of the string (not from middle)
+
+            12. str.contains("somestring") : retuns true if "somestring" is found,else found
+
+            13. str.startsWith()
+                    str.startsWith("C") - return true if str start with C
+                    str.startsWith("t") - return false if str doesnt start with t
+                    str.startsWith("k",4) - return true if str starts with k starting the search from index 4
+            
+            14. str.endsWith()
+                    same as above
+            
+            15. .format() 
+                to format the string 
+
+                %d 
+                %t
+                %x 
+                %c, etc
+
+            16.  getBytes() - covertes string into asci value for each character and will store in tinot a character
+
+            17.  getChars() - copy content from a string into character array 
+
+
+            18. .empty()
+            19. .join()
+                    eg. str.join(":","9","10","11") ===> 9:10:11
+                    eg. str.join(":","9","10","11") ===> 9:10:11
+                    eg. str.join(":","hello hi") ===> hello hi
+
  */
 class StringsIntro{
 
@@ -55,10 +110,35 @@ class StringsIntro{
         // String s2 = "hello";
         String s2 = s1;
 
+        /*
+          till here what is happening is :- 
+
+
+
+            s1 ------------>("hello")
+                            /
+            s1 ------------/
+
+            
+         */
+
         s1 = "hi";
         System.out.println(s1);
         System.out.println(s2);
 
+
+        /*
+         now :- 
+
+                        ("hi") 
+                        /
+            s1 --------/    ("hello")
+                            /
+            s1 ------------/
+
+            
+         */
+         */
         
     }
     
