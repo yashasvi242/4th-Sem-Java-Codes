8813e7667c79da635be365648806af9afb3143fb
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:14:09 +0530

    Create String (12).java

---
+++ Till ST2/Strings/String (12).java
@@ -0,0 +1,11 @@
+// converts specified type into 'String' objext 
+
+/* Common Format Specifiers
+ *
+ * %s - String value
+ * %d - Integer value
+ * %f - Decimal value (floating point)
+ * %x - Hextstring value from integer
+ * 
+ */
+String.format("String with format specifiers here: %d %s %f %x", 10, "input", 2.7, 26); 
