52d903233d1b2ed01399934c21d0d2276462b8e5
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:28:17 +0530

    Create javaStringTokenizerINTRO.java

---
+++ Till ST2/Java String Tokenizer/javaStringTokenizerINTRO.java
@@ -0,0 +1,12 @@
+
+public class intro {
+
+    /* Tokens in Java are :-
+    
+        - used to break string into several parts
+        - using delimiter
+        - java.util.StringTokernizer;  
+
+     */
+    
+}
