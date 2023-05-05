7cdc544be69c5281070e3bc2cb62a58436173cdf
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:08:05 +0530

    Create string (8).java

---
+++ Till ST2/Strings/string (8).java
@@ -0,0 +1,14 @@
+public class StrSubstr{
+
+    public static void main(String args[]){
+        String str = "Java Programming";
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(5,7)); // means start from index '5' and end at index '6' ---> pr
+    }
+    
+}
+
+/*
+ Programming
+ Pr
+ */
