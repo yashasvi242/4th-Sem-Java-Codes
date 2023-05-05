a5d82a01ef0c6c2c6e0f6a5dc54a9f097d185782
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:11:49 +0530

    Create String (11).java

---
+++ Till ST2/Strings/String (11).java
@@ -0,0 +1,17 @@
+
+public class Trim {
+    
+    // .Trim() removes the spaces from start and end of the String
+    
+
+    public static void main(String args[]){
+        String str = "   Remove   WhiteSpaces     ";
+        System.out.println(str.trim());
+    }
+}
+/*
+ Output :- 
+
+    Remove   WhiteSpaces
+    
+ */
