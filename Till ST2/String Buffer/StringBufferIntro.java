c0fa09aac76c1538e747b5cbad348a5dacc7a8bc
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 09:24:38 +0530

    Create StringBufferIntro.java

---
+++ Till ST2/String Buffer/StringBufferIntro.java
@@ -0,0 +1,16 @@
+/*
+ String buffer is used to create a mutable string objext, it means it can be changed after it is creater.
+ It represents growable and writable character sequence
+
+ It is similar to string class in java both are used to create string but string buffer object can be changed
+
+ It is used when we make lot of modifications to out string 
+
+ String buffer defined 4 constructors.
+
+    - 1. StringBuffer();   - stores 14 characters space for itslelf 
+    - 2. StringBuffer(int size);    
+    - 3. StringBuffer(String str);
+    - 4. StringBuffer(charSequence[] ch); 
+  
+*/
