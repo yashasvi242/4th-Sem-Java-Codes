eaa6d0ed931038384473438d87b98d03238a6708
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:10:33 +0530

    Create string (10).java

---
+++ Till ST2/Strings/string (10).java
@@ -0,0 +1,19 @@
+
+public class toStringFun {
+
+    public static void main(String args[]){
+        toStringFun c = new toStringFun();
+        System.out.println(c);
+    }
+
+    public String toString(){
+        return "This String Will return to car object";
+    }
+    
+}
+
+/*
+ Output : 
+    This String Will return to car object
+    
+ */
