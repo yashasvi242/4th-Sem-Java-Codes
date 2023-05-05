f61dc1fe4bf4c32e422a53d20a45e12a3b87a77f
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 09:53:53 +0530

    Create string (5).java

---
+++ Till ST2/String Buffer/string (5).java
@@ -0,0 +1,11 @@
+public class StrBuff4 {
+
+    public static void main(String args[]){
+        StringBuffer str = new StringBuffer("hello");
+        str.reverse();
+        System.out.println(str);
+    }
+}
+
+// olleh
+
