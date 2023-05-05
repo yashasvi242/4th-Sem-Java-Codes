9f557e776841ee922ddc75e2d7f4ad3f8abe9316
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:26:18 +0530

    Create String Builder(1).java

---
+++ Till ST2/String Builder/String Builder(1).java
@@ -0,0 +1,30 @@
+/*
+  String Builder 
+
+    - it is similar to string buffer
+    - only diff is it is not synchronized
+    - it is not thread safe
+    - mutable string objexts 
+    - use this if synchronization is not required
+    - java.lang package 
+    - use this if we want to save the thread or give multiple threads the access.
+
+
+    String Builder constrcutors 
+        StringBuilder() - created space for 16 character i.e capacity
+        StringBuilder(str)
+        StringBuilder(String str)
+
+    It has all the methods similar to 'StringBuffer'
+
+        .append()
+        .insert()
+        .replace()
+        .reverse()
+        
+
+
+ */
+public class StringBuilderIntro {
+    
+}
