066253066f727430d40ac669efd6241916ed3eb0
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-05 15:18:57 +0530

    Create String (15).java

---
+++ Till ST2/Strings/String (15).java
@@ -0,0 +1,11 @@
+public class StringToCharArrayExample{  
+   public static void main(String args[]){  
+      String s1="hello";  
+      char[] ch=s1.toCharArray();  
+      for(int i=0;i<ch.length;i++){  
+      System.out.print(ch[i] + ", ");  
+      }  
+   }  
+}  
+
+// h, e, l, l, o, 
