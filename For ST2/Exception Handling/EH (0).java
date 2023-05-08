025131668d5871a55a32cb70a7ce96ceb08c2dbc
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-08 10:23:23 +0530

    Create EH (0).java

---
+++ For ST2/Exception Handling/EH (0).java
@@ -0,0 +1,17 @@
+
+/*
+     Exception Handling in java to handle exception(error) at runtime.
+     If we do not habdke the exceptions, it leads to unexpected program terminaltion.
+
+     Default Exception - JVM
+
+        1. Uncaught Exception 
+
+        Types of Exceptions :- 
+            1. Checked Exception  - at compile time
+            2. Unchecked  - at runtime 
+            3. Error Based -nothign can be done about it - eg. segmentation fault, stack overflow 
+
+
+
+*/
