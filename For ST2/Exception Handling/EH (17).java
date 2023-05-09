5411e7345f4c1985fc33bf86cc147d9c48458875
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-09 16:06:16 +0530

    custom exception handler class

---
+++ For ST2/Exception Handling/EH (17).java
@@ -0,0 +1,37 @@
+
+// Creating custom 'MyException' so that we can overwrite the toString() (math.lang) of default JVM exception handler
+class MyException extends Exception{
+
+    private int ex;  // 3.then prog reaches here with val -10
+    MyException(int a){ 
+        ex = a;
+    }
+    public String toString(){
+        return "My Exception[" + ex + "] is less then ZERO"; 
+    }
+}
+
+class Demo{
+    static void sum(int a, int b) throws MyException{
+        if(a < 0){
+            throw new MyException(a);  // 2. from here prog jumps to class 'MyException' and its objext will be created, and its contructor(-10) will be called 
+                                    // 4. from here prpg jumps to catch block (to resolve the exception)
+        }
+        else{
+            System.out.println(a+b);
+        }
+    }
+
+    public static void main(String ... args) {
+
+        try{
+            sum(-10,10); // 1.from here prg jumps to line 14 
+        }
+        catch(MyException me){
+            System.out.println(me);   // 5. now toString() of the 'MyException class is printed '
+        }
+    }
+}
+
+// Output :- 
+// My Exception[-10] is less then ZERO
