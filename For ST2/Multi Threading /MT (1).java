37a80bf392fc5891c0d2465d0975f742eb76ea37
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-10 22:01:02 +0530

    Thread and Runnable intro

--- For ST2/Multi Threading /MT (1).java
+++ For ST2/Multi Threading /MT (1).java
@@ -1,3 +1,15 @@
+
+/*
+There are 2 ways to create Threads
+    1. using Throw class
+    2. using Runnable interface
+    
+    Throw class has a lot of methods and contructors that are predefined eg. .run(), .start(), .isAlive(), .setName(), ....
+    Runnable interface has only 1 method .run() 
+    
+    so runnable interface should be used if we only want to create a thread, but if not then use 'Throw' class
+
+*/
 class MultiThreading{
 
     public static void main(String ... args){
