1adc9e085cfd1de692587df79ef544cfbdf06409
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-10 22:19:54 +0530

    Update MT (6).java

--- For ST2/Multi Threading /MT (6).java
+++ For ST2/Multi Threading /MT (6).java
@@ -1,4 +1,12 @@
-
+/*
+VIMP Note: at line 29, thread t1 will be called and returned at the same time, now t1 is simultaneously running in its own call stack, and immediately t2 is called, and this way they both 
+            are now running simultaneously, thats why we can never guess the output of the program, coz it totaly depends on the processor and not on how u invoke the run by .start()
+            but if we directly invoke .run() without using .start(), then the t1 thread will be completed first, then the t2 will be completed afterwards the completion of thread t1.
+            
+            in Multithreading multiple stacks are there 
+            in Single Threading no multiple stacks exist, there is only 1 stack i.e of the main thread
+    
+*/
 public class MyThread extends Thread{
 
     String task;
@@ -23,7 +31,7 @@ public class MyThread extends Thread{
         MyThread th1 = new MyThread("Cut the ticket");
         MyThread th2 = new MyThread("show your seat number");
 
-        Thread t1 = new Thread(th1);
+        Thread t1 = new Thread(th1);  // read note at the top
         Thread t2 = new Thread(th2);
         t1.start();//1.
         t2.start();//2.
