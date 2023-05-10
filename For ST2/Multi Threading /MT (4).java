40829be3d064913b3cfe1ffedefac17e8fe08d70
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-10 22:08:34 +0530

    Update MT (4).java

--- For ST2/Multi Threading /MT (4).java
+++ For ST2/Multi Threading /MT (4).java
@@ -8,7 +8,7 @@ class MyThread extends Thread{//1. extended 'Thread' because 'Thread' already ha
 class MyThreadDemo1{
     public static void main(String ... args) {
 
-        MyThread mt = new MyThread();
+        MyThread mt = new MyThread();  // here this mt will have all the fun of Thread coz it has extended that class
         mt.start();
         
     }
