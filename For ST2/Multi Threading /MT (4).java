e952e53ee8c1d128e946f42611ade2dd6e7d1019
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-10 14:26:18 +0530

    Create MT (4).java

---
+++ For ST2/Multi Threading /MT (4).java
@@ -0,0 +1,18 @@
+
+class MyThread extends Thread{//1. extended 'Thread' because 'Thread' already has a lot of methods (not like Runable that has only .run in it)
+
+    public void run(){
+        System.out.println("concurrent thread started running..");
+    }
+}
+class MyThreadDemo1{
+    public static void main(String ... args) {
+
+        MyThread mt = new MyThread();
+        mt.start();
+        
+    }
+}    
+
+
+// concurrent thread started running..
