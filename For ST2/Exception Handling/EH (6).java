deb352fdbdaed06335a3bbf45c56cc7c40b6eb95
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-10 21:46:27 +0530

    Update EH (6).java

--- For ST2/Exception Handling/EH (6).java
+++ For ST2/Exception Handling/EH (6).java
@@ -4,7 +4,8 @@ public class EH6 {
     public static void main(String ... args){
         try{
             int arr[] = {1,2};
-            arr[2] = 3/0;
+            arr[2] = 3/0;    // altough only considering this line, runtime error for div by 0 will hit first, coz in this value of 3/0 is going to get assigned to arr[2] so that means 3/0 is getting calculated first, that will throw error 
+ 
         }
         catch(Exception e){
             System.out.println("Generic Exception");
