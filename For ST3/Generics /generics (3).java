41fed29ea02b167c2739ede719aef17e62920174
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-22 10:00:17 +0530

    Create generics (3).java

---
+++ For ST3/Generics /generics (3).java
@@ -0,0 +1,20 @@
+public class Demo2 {
+
+
+    public static void main(String ... args){
+
+        Gen<Integer> iob1 = new Gen(100);
+        int x = iob1.getObj();
+        System.out.println(x);
+
+        Gen<Integer> iob2 = new Gen(200);
+        iob2 = iob1;  // we can assign like this only for objexts for same type like we cant store a String obj in an int
+        int y = iob2.getObj();
+        System.out.println(y);
+    }
+    
+}
+
+// Output :- 
+// 100
+// 100
