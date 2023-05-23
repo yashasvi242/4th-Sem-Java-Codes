6d632ae6468e32a8dc3847513ef4dd94d44496e4
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-23 15:43:51 +0530

    Update arrayList (1).java

--- For ST3/Java Collections Framework (JCF)/ArrayList/arrayList (1).java
+++ For ST3/Java Collections Framework (JCF)/ArrayList/arrayList (1).java
@@ -1,4 +1,13 @@
-import java.util.*;
+ import java.util.*;
+/*
+    ArrayList 
+    ---------
+
+    .get(index)
+    .remove(index)
+    .indexOf(ele) - first occ
+    .lastIndexOf(ele) - last occ
+*/
 
 class Demo{
 
@@ -11,6 +20,9 @@ class Demo{
         arr.add(30);
         arr.add(40);
         arr.add(50);
+        arr.add(50);
+        arr.add(50);
+        arr.add(020); // here '020' (hexadecimal) gets converted to int -> 16 
 
         System.out.println(arr); // printing object
 
@@ -18,12 +30,23 @@ class Demo{
         for(int i=0; i < arr.size(); i++){
             System.out.print(arr.get(i) + " ");
         }
+
+        arr.remove(1); // deleting index 1 element 
+
+        System.out.print("\n" + arr);
+
+        System.out.println("\n 1st occ of 50 is at index : " + arr.indexOf(50));
+        System.out.println("last occ of 50 is at index : " + arr.lastIndexOf(50));
     }
 }
 
 /* Output :- 
 
-[10, 20, 30, 40, 50]
+[10, 20, 30, 40, 50, 50, 50, 16]
 printing array elements 1 by 1
-10 20 30 40 50 
+10 20 30 40 50 50 50 16 
+[10, 30, 40, 50, 50, 50, 16]
+ 1st occ of 50 is at index : 3
+last occ of 50 is at index : 5
+
  */
