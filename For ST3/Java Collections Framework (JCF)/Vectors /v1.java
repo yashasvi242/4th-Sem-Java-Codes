62bb39bc868dc74d00e00d605a5385348e5e9453
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-05-25 11:23:36 +0530

    Update v1.java

--- For ST3/Java Collections Framework (JCF)/Vectors /v1.java
+++ For ST3/Java Collections Framework (JCF)/Vectors /v1.java
@@ -2,3 +2,31 @@
 // similar to ArrayList
 // it is thread safe (unlike arrayList)
 
+import java.util.*;
+public class Vectors {
+    
+
+    public static void main(String[] args){
+        Vector<Integer> v1 = new Vector<>();
+
+        v1.add(10);
+        v1.add(20);
+        v1.add(30);
+        v1.addElement(40);// same as .add()
+        v1.addElement(9);
+        v1.addElement(8);
+        v1.addElement(7);
+
+        System.out.println(v1);
+
+        for(int i=0; i < v1.size(); i++){
+            System.out.print(v1.get(i) + " | ");
+        }
+
+
+    }
+}
+
+
+// [10, 20, 30, 40, 9, 8, 7]
+// 10 | 20 | 30 | 40 | 9 | 8 | 7 |
