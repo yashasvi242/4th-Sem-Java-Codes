f16b3785e9a07d11272fe20b0a491f7f3792dca2
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-06-05 20:49:56 +0530

    Create hashset.java

---
+++ For ST3/Java Collections Framework (JCF)/Set/hashset.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+// HASHSET (java) -> unorderd_set (cpp)
+// it is unsorted
+// stores only distinct values
+
+// .add()
+// .size()
+
+public class Q2 {
+
+    public static void main(String ... args){
+
+        HashSet<String> hs = new HashSet<>();
+
+        // hashmap  - > unordered set (stores only distinct values)
+
+        Scanner scn = new Scanner(System.in);
+        int size = scn.nextInt();
+
+        for(int i=0; i < size; i++){
+            String word = scn.next();
+            hs.add(word);
+        }
+        System.out.print("No of distinct words : " + hs.size());
+    }
+}
