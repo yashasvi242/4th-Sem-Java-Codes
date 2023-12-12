59759f6d44219bac63263d98192c279f2d837ecf
Yashasvi Yadav <103374718+yashasvi242@users.noreply.github.com>
2023-12-12 10:11:18 +0530

    Add files via upload

---
+++ Coding Material for Programming Abstractions ETE/HeapsInJava.java
@@ -0,0 +1,22 @@
+
+// task given an array of integers, u need to create a minHeap and maxHeap
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int arr[] = {10,15,12,13,14};
+		
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		for(int i=0; i < arr.length; i++){
+		    minHeap.add(arr[i]);
+		    maxHeap.add(arr[i]);
+		}
+		
+		// printing min and max heaps 
+		System.out.println("maxHeap : " + maxHeap);
+		System.out.println("minHeap : " + minHeap);
+	}
+}
